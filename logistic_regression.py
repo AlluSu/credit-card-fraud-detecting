72aa51e2cc04abfd9f211baae00693aa020638a5
Tuomas Jani Rudolf Laakso <tuomasl@Tuomass-MacBook-Pro.local>
2025-10-15 15:55:05 +0300

    changed log_reg to input prepared data

--- logistic_regression.py
+++ logistic_regression.py
@@ -6,47 +6,9 @@ from sklearn.metrics import classification_report, confusion_matrix, accuracy_sc
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def logistic_regression():
-    
-    # This assumes that the file exists in the same folder as this script
-    # The file is quite large
-    df = pd.read_csv("creditcard.csv")
+def logistic_regression(X, y, smote=False):
 
-    # Check the schema of the data
-    df.head()
-
-    # Check the types of data
-    df.info()
-
-    # Our target variable is the last one, "Class", which determines if the transaction is fraud or not, i.e, 1 or 0
-    # Occurrences of fraud vs. non-fraud
-
-    occurrences = df['Class'].value_counts()
-    occurrences
-
-    # Ratio of fraudulent cases
-    ratio = (df["Class"] == 1).sum() / len(df["Class"])
-    print("Ratio of fraudulent vs. non-fraudulent cases: \n", ratio)
-
-    # Create a feature vector from the columns V1, .., V28
-    X = df.loc[:, "V1":"V28"]
-    X
-
-    X = df.loc[:, "V1":"V28"].values
-    X
-
-    # Set the "Class" column as the target variable
-    y = df["Class"].values
-    y
-
-    # Split into training and testing sets, use 80/20 ratio
-    X_train, X_test, y_train, y_test =  train_test_split(X, y, test_size=0.2)
-
-    # Fit logistic regression model
-    # Use liblinear as the solver for now, as it is compatible with a binary classification problem
-    # liblinear also supports both L1 and L2 regularization
-    # Later on the project we can try with other different parameters
-    # https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html#logisticregression
+    X_train, X_test, y_train, y_test =  train_test_split(X, y, test_size=0.05)
 
     model = LogisticRegression(solver="liblinear")
     regression = model.fit(X_train, y_train)
@@ -84,5 +46,3 @@ def logistic_regression():
 
     recall = true_positive / (true_positive + false_negative)
     print("Recall: ", recall)
-
-logistic_regression()
--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from LoadNPrep import data_loader
 from random_forest import random_forest, plot_roc_pr
+from logistic_regression import logistic_regression
+
+def RF_drive(X,y, smote_trigger):
+    metrics = random_forest(X,y, smote_trigger=False)
+    proba = metrics['proba'] if 'proba' in metrics else None
+    y_test = metrics['y_test'] if 'y_test' in metrics else None
+    plot_roc_pr(y_test, proba)
 
 
 
 def driver():
+
     X,y = data_loader()
-    metrics = random_forest(X,y smote_trigger=False)
-    proba = metrics['proba'] if 'proba' in metrics else None
-    y_test = metrics['y_test'] if 'y_test' in metrics else None
-    plot_roc_pr(y_test, proba)
+    logistic_regression(X, y, smote=False)
+
+    # For none smote
+    RF_drive(X,y, False) 
+
+    # For smote
+    # RF_drive(X,y, True)
 
 
 
